ef2fa769e778afd4c46cfca993ba9ba32c044d90
shinjaehoon <noahshin11@gmail.com>
2024-01-03 15:16:33 +0900

    22:07

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,64 @@
-use std::net::SocketAddr;
-use axum::response::{Html, IntoResponse};
-use axum::Router;
-use axum::routing::get;
+pub use self::error::{Error, Result};
+use axum::extract::{Path, Query};
 
+use axum::response::{Html, IntoResponse, Response};
+use axum::routing::{get, get_service};
+use axum::{middleware, Router};
+use serde::Deserialize;
+use std::net::SocketAddr;
+use tower_http::services::ServeDir;
+
+mod error;
+mod web;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route("/hello", get(handler_hello),
-    );
+    let routes_all = Router::new()
+        .merge(routes_all())
+        .merge(web::routes_login::routes())
+        .layer(middleware::map_response(main_response_mapper))
+        .fallback_service(route_static());
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("->> LISTENING on {addr}\n");
 
     axum::Server::bind(&addr)
-        .serve(routes_hello.into_make_service()).await.unwrap();
-
+        .serve(routes_all.into_make_service())
+        .await
+        .unwrap();
 }
 
-async fn handler_hello() -> impl IntoResponse {
-    println!("->> {:<12} - handler_hello", "HANDLER");
+async fn main_response_mapper(res: Response) -> Response {
+    println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
 
-    Html("Hello <strong>World!!!</strong>")
+    println!();
+    res
+}
+
+fn route_static() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./")))
+}
+fn routes_all() -> Router {
+    Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello2))
+}
+
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
+
+    let name = params.name.as_deref().unwrap_or("World!");
+
+    Html(format!("Hello <strong>{name}</strong>"))
+}
+
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello2 - {name:?}", "HANDLER");
+
+    Html(format!("Hello2 <strong>{name}</strong>"))
 }
